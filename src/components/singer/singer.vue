c09b09eb584f6f65df6acf8c0114c3cc7c5fa891
xmt <xmtchristine@163.com>
2020-10-23 21:01:00 +0800

    'singer'

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -3,21 +3,58 @@
 </template>
 
 <script>
-import { onMounted } from "vue";
+import { onMounted, reactive } from "vue";
 import { hotSinger, letterSinger } from "api/singer.js";
 export default {
   setup() {
-      const state = {
-          hotList:[]
-      }
+    const HOT_NAME = '热门'
+    const state = reactive({
+      singers: [],
+    });
+
     onMounted(() => {
       hotSinger().then((res) => {
-        console.log("====hot", res);
+        console.log(res);
+        if (res.statusText === "OK" && res.status === 200) {
+          console.log("33333");
+          res.data.artists.map((curobj, index) => {
+            state.hot.push({
+              picUrl: curobj.picUrl,
+              name: curobj.name,
+            });
+          });
+          console.log(state.hot);
+        }
+      });
+      letterSinger("a").then((res) => {
+        console.log("====a", res);
       });
     });
-    letterSinger("a").then((res) => {
-      console.log("====a", res);
-    });
+    
+    const normalizaSinger=()=>{
+      let map = {
+        hot: {
+          title:HOT_NAME,
+          items:[]
+        }
+      }
+      //处理热门歌手数据
+      hotSinger().then((res) => {
+        if (res.statusText === "OK" && res.status === 200) {
+          res.data.artists.map((curobj, index) => {
+            map.hot.items.push({
+              avatar: curobj.picUrl,
+              name: curobj.name,
+            });
+          });
+        }
+      })
+      
+      // 处理26个字母的歌手列表
+
+    }
+  
+  
   },
 };
 </script>
